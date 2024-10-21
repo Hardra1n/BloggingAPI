d2d2f846b44baa35dd3df30fef40c34b44755822
Kiryl Kho Van <whiteboywithnotattoos@gmail.com>
2024-10-21 17:19:02 +0300

    (feat) Add constraints EF attributes to model

---
+++ src/BlogAPI/Migrations/20241021141641_addConstraintsToPosts.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class addConstraintsToPosts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Posts",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Posts",
+                type: "character varying(4000)",
+                maxLength: 4000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Posts",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Posts",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(4000)",
+                oldMaxLength: 4000);
+        }
+    }
+}
--- src/BlogAPI/Models/Post.cs
+++ src/BlogAPI/Models/Post.cs
@@ -1,7 +1,19 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 public class Post
 {
+    [Key]
     public int PostId { get; set; }
+
+    [Required]
+    [MaxLength(30)]
     public string Title { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(4000)]
     public string Content { get; set; } = string.Empty;
+
+    [Required]
     public DateTime CreatedAt { get; set; }
 }
