1badff4b291dda458c7f2b4a76545579ac44d349
Kiryl Kho Van <whiteboywithnotattoos@gmail.com>
2024-10-21 16:57:28 +0300

    (feat) Add data validation on model binding

--- src/BlogAPI/Dtos/PostCreateDto.cs
+++ src/BlogAPI/Dtos/PostCreateDto.cs
@@ -1,6 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BlogAPI.Dtos;
-public record PostCreateDto(string Title, string Content)
+public record PostCreateDto
 {
+
+    public PostCreateDto(string title, string content)
+    {
+        Title = title;
+        Content = content;
+    }
+
+
+    [Required]
+    [Length(1, 30)]
+    public string Title { get; set; } = string.Empty;
+
+    [Required]
+    [Length(1, 4000)]
+    public string Content { get; set; } = string.Empty;
+
+
     public Post ToPost()
     {
         return new Post()
--- src/BlogAPI/Dtos/PostUpdateDto.cs
+++ src/BlogAPI/Dtos/PostUpdateDto.cs
@@ -1,11 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BlogAPI.Dtos;
-public record PostUpdateDto(int PostId, string Title, string Content)
+public record PostUpdateDto
 {
+    public PostUpdateDto(int postId, string title, string content)
+    {
+        PostId = postId;
+        Title = title;
+        Content = content;
+    }
+
+
+    [Required]
+    public int? PostId { get; set; }
+
+    [Required]
+    [Length(1, 30)]
+    public string Title { get; set; } = string.Empty;
+
+    [Required]
+    [Length(1, 4000)]
+    public string Content { get; set; } = string.Empty;
+
+
     public Post ToPost()
     {
         return new Post
         {
-            PostId = PostId,
+            PostId = (int)PostId!,
             Title = Title,
             Content = Content
         };
